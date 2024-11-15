f6a5a70f27e6000c3f867c000b2dcc731b02e6d1
Martín Pravia <marto.pravia@gmail.com>
2024-11-15 20:15:38 +0000

    creo la home y componentes que aun se encuentran vacíos

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,43 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  return (
+    <div className="App container py-4">
+      <h1 className="text-center text-info">Tic Tac Toe</h1>
+      <h2 className="text-center text-danger">Pick A Weapon</h2>
+      <div className="gameSetting bg-warning-subtle p-5">
+        <div className="row selector justify-content-center text-center  d-flex">
+          <h2>Choose Your Weapon</h2>
+          <div className="col-4 justify-content-center">
+            {" "}
+            <input
+              type="text"
+              placeholder="Player 1 username"
+              className="form-control my-2"
+            />
+          </div>
+          <div className="col-4 align-items-center">
+            <input
+              type="text"
+              placeholder="Player 2 username"
+              className="form-control my-2"
+            />
+          </div>
+          <div className=" row btn-group  ">
+			<div className="col-2"></div>
+            <div className="col-4 mx-auto">
+              <button className="button btn btn-primary btn-lg fs-2">X</button>
+            </div>
+            <div className="col-4 mx-auto">
+              <button className="button btn btn-info btn-lg fs-2">O</button>
+            </div>
+			<div className="col"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
